e12943ee2a4d588eb6a8ae32c15f2ddd7a0d221d
Instel <135399235+Instel12@users.noreply.github.com>
2025-04-22 15:38:49 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,22 @@ set -e
 
 # Update and install required packages
 sudo apt update
-sudo apt install -y firefox xfce4 xfce4-goodies tightvncserver git wget
+#!/bin/sh
+clear
+echo ⭐ Welcome to the Clinux setup! ⭐
+echo "❓ Would you like to install Firefox? (y/n) ❓"
+read answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+    sudo apt install -y firefox xfce4 xfce4-goodies tightvncserver git wget
+fi
+clear
+echo "❓ Would you like to install 7-zip? (y/n) ❓"
+read answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+    sudo apt install -y p7zip-full
+fi
+
+
 
 # Set up VNC server
 vncserver :1
@@ -34,3 +49,5 @@ vncserver :1
 
 # Start noVNC on port 6080
 "$NOVNC_DIR"/utils/novnc_proxy --vnc localhost:5901 --listen 6080 &
+clear
+echo ⭐ Finished! You may now kill this terminal. ⭐
